349f1b2bedad56f9799078a44faefbcb031b0e68
deepaksundar <deepaksundar.95@gmail.com>
2018-08-10 16:58:06 +0530

    Code clean up - removed the unwanted code
    the removed class GroupInitializer was created for testing purpose

--- src/main/java/com/example/productprice/ProductPriceManagementApplication.java
+++ src/main/java/com/example/productprice/ProductPriceManagementApplication.java
@@ -23,45 +23,4 @@ public class ProductPriceManagementApplication {
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
-
-	/*
-	 * @Bean public UserDetailsService userDetailsService() throws Exception { //
-	 * ensure the passwords are encoded properly
-	 * 
-	 * UserBuilder users = User.withDefaultPasswordEncoder();
-	 * InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-	 * manager.createUser(users.username("user").password("password").roles("USER").
-	 * build());
-	 * manager.createUser(users.username("admin").password("password").roles("USER",
-	 * "ADMIN").build()); return manager; }
-	 */
-}
-
-@Component
-class GroupInitializer implements CommandLineRunner {
-
-	private final ProductPriceRepository productPriceRepository;
-
-	GroupInitializer(ProductPriceRepository productPriceRepository) {
-		this.productPriceRepository = productPriceRepository;
-	}
-
-	@Transactional
-	void createProduct(ProductPrice productPrice) {
-		ProductPrice response = productPriceRepository.save(productPrice);
-		System.out.println("success  " + response.getId() + "  " + response.getPrice().getValue() + "  "
-				+ response.getPrice().getCurrencyCode());
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-
-		// Create Application
-		// createApplication(Constants.application_AccessManagementSystemMainApplication);
-		// createApplication(Constants.application_TestApplicationToManageResourceTypeOrder);
-		Price price = new Price(9.99, "INR");
-		ProductPrice productPrice = new ProductPrice(1235, price);
-		// createProduct(product);
-
-	}
 }
